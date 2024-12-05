1766557dde4a118fb916c2b89adc40bccd94ba38
Rudy1147 <RodolfoHernandez661@gmail.com>
2024-12-04 22:45:27 -0600

    MessageActivity with the friends.csv file

--- app/src/main/java/edu/utsa/cs3443/whoson/MessageActivity.java
+++ app/src/main/java/edu/utsa/cs3443/whoson/MessageActivity.java
@@ -1,9 +1,13 @@
 package edu.utsa.cs3443.whoson;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +15,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+
 public class MessageActivity extends AppCompatActivity {
+
+    private EditText Enter_Text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +35,7 @@ public class MessageActivity extends AppCompatActivity {
             return insets;
         });
 
-
+        Enter_Text = findViewById(R.id.MessageText);
 
         ImageButton fpHomeButton = findViewById(R.id.m_home_button);
         //  OnClickListener
@@ -41,5 +52,32 @@ public class MessageActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        ImageButton send_button = findViewById(R.id.SendButton);
+        // OnClickListener
+        send_button.setOnClickListener(v -> {
+            String name = Enter_Text.getText().toString();
+            WriteMessage(this, name);
+        });
+
     }
+
+    public void WriteMessage(MessageActivity activity, String message)
+    {
+        try {
+
+            File file = new File("assets/messagelog.csv"); // Looks up file
+            // Open the CSV file
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(message); // Writes the message into the file.
+            bw.newLine();
+            Toast.makeText(this, "Message successfully sent!", Toast.LENGTH_SHORT).show();
+            bw.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+              Toast.makeText(this, "Error writing into CSV file.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
